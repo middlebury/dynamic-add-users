4250400e6129c3abea389ef060f3c613303c01f0
Adam Franco <afranco@middlebury.edu>
2023-08-01 12:04:37 -0400

    Bump version to 1.2.2

--- dynamic-add-users.php
+++ dynamic-add-users.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Dynamic Add Users
  * Plugin URI:        https://github.com/middlebury/dynamic-add-users
  * Description:       Replaces the 'Add User' screen with a dynamic search for users and groups.
- * Version:           1.2.1
+ * Version:           1.2.2
  * Author:            Middlebury College
  * Author URI:        https://github.com/middlebury/
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'DYNAMIC_ADD_USERS_VERSION', '1.2.0' );
+define( 'DYNAMIC_ADD_USERS_VERSION', '1.2.2' );
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 
